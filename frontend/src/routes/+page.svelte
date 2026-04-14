50e3d21adf4569c8c8f0c1b5a660571b09a3a6f8
hegik <hegik@me.com>
2026-04-14 10:19:35 +0200

    fixed the logo

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -71,7 +71,8 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 0.75rem 1.25rem;
+    padding: 0 1.25rem;
+    height: 32px;
     background: #e8500a;
     z-index: 10;
   }
