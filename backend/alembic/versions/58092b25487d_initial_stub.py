1f28ad278eb6281c6814930dd3bd37f19f9739de
hegik <hegik@me.com>
2026-04-15 13:18:25 +0200

    fixed Moderation panel 3

---
+++ backend/alembic/versions/58092b25487d_initial_stub.py
@@ -0,0 +1,24 @@
+"""initial state stub
+
+Revision ID: 58092b25487d
+Revises:
+Create Date: 2025-01-01
+
+This is a placeholder for the initial database state that was created
+outside of Alembic migrations. The schema already exists in the database.
+"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '58092b25487d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
